32910193dd7882b4e1ce78484e8df1f51b11b290
tselmeg19 <tseeme0102@gmail.com>
2025-01-29 13:58:10 +0800

    amarcilsan

---
+++ das1.py
@@ -0,0 +1,7 @@
+course_id = input("хичээлийн код: ")
+course_name = input("хичээлийн нэр: ")
+credit = input("хичээлийн кредит: ")
+madlib = " энэ хичээлийн код нь {}".format(course_id) + \
+", хичээлийн нэр нь {}".format(course_name).title() + \
+" бөгөөд уг хичээл нь {}".format(credit) + " кредитийн хичээл юм."
+print(madlib)
